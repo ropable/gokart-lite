5e0284644c0c4f4122737b6d60a2e0b139e05129
Rocky Chen <Rocky.Chen@dbca.wa.gov.au>
2021-09-13 09:15:14 +0800

    Check whether the domain of the host page is the same as the domain of the embedded map

--- src/gokart.js
+++ src/gokart.js
@@ -3,7 +3,11 @@ import {getCRS} from './components/crs.js'
 import {Layer} from './components/layer.js'
 import {utils} from 'src/vendor.js'
 
-var Gokart = function(app,mapid,embeded) {
+var serviceUrlRe = new RegExp('^(?<service>([htps]+:\\/\\/)?[a-z0-9_\\.\\-]+(:[0-9]+)?)(?<path>(\\/.*)?$)','i')
+
+var domainRe = new RegExp('^((?<protocol>([htps]+)):\\/\\/)?(?<app>[a-z0-9_\\-]+)((?<domain>\\.[a-z0-9_\\.\\-]+))?(:(?<port>[0-9]+))?$','i')
+
+var Gokart = function(app,mapid,embeded,gokartService) {
     //initialize env
     this.env = eval(app + "Env")
     if (typeof gokartOptions !== 'undefined') {
@@ -21,6 +25,34 @@ var Gokart = function(app,mapid,embeded) {
     this.env["app"] = app
     this.env["mapid"] = mapid
     this.embeded = embeded?true:false
+    try {
+        if (this.embeded) { 
+            if (gokartService) {
+                this.gokartService = gokartService
+                this.gokartDomain = this.gokartService.match(domainRe).groups["domain"] || null
+                
+                if (this.gokartDomain) {
+                    this.sameDomain = (window.top.location.host.endsWith(this.gokartDomain))?true:false
+                } else {
+                    this.sameDomain = false
+                }
+                this.sameDomain = (window.top.location.host.endsWith(this.gokartDomain))?true:false
+            } else {
+                this.gokartService = undefined
+                this.gokartDomain = undefined
+                this.sameDomain = false
+            }
+        } else {
+            this.gokartService = window.location.toString().match(serviceUrlRe).groups["service"]
+            this.gokartDomain = this.gokartService.match(domainRe).groups["domain"] || null
+            this.sameDomain = (this.gokartDomain)?true:false
+        }
+    } catch (ex) {
+        this.gokartService = undefined
+        this.gokartDomain = undefined
+        this.sameDomain = false
+    }
+     
 
     var vm = this
     $.each([["publicWmtsService","wmtsService"],["publicWmsService","wmsService"],["publicWfsService","wfsService"]],function(index,config){
@@ -30,25 +62,31 @@ var Gokart = function(app,mapid,embeded) {
     })
     
     //try to authenticate user
-    $.ajax({
-        url: this.env["whoamiUrl"],
-        method:"GET",
-        dataType:"json",
-        success: function (response, stat, xhr) {
-            vm.user = response
-            vm.user["authenticated"] = vm.user["session_key"]?true:false
-            //create leaflet map
-            vm.map = new Gokart.Map(vm)
-        },
-        error: function (xhr,status,message) {
-            vm.user = {authenticated:false}
-            //create leaflet map
-            vm.map = new Gokart.Map(vm)
-        },
-        xhrFields: {
-          withCredentials: true
-        }
-    })
+    if (this.sameDomain) {
+        $.ajax({
+            url: this.env["whoamiUrl"],
+            method:"GET",
+            dataType:"json",
+            success: function (response, stat, xhr) {
+                vm.user = response
+                vm.user["authenticated"] = vm.user["session_key"]?true:false
+                //create leaflet map
+                vm.map = new Gokart.Map(vm)
+            },
+            error: function (xhr,status,message) {
+                vm.user = {authenticated:false}
+                //create leaflet map
+                vm.map = new Gokart.Map(vm)
+            },
+            xhrFields: {
+              withCredentials: true
+            }
+        })
+    } else {
+        this.user = {authenticated:false}
+        //create leaflet map
+        this.map = new Gokart.Map(vm)
+    }
 }
 
 Gokart.prototype.isAuthenticated = function() {
